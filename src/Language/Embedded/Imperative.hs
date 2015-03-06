8c2b249d96905f568babde7ab3816cd0cad7e116
Emil Axelsson <emax@chalmers.se>
2015-03-06 15:06:58 +0100

    Implemented programs with references

---
+++ src/Language/Embedded/Imperative.hs
@@ -0,0 +1,156 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Language.Embedded.Imperative where
+
+
+
+import Data.IORef
+import Data.Typeable
+
+import Control.Monad.Operational
+import Data.Constraint
+import Language.C.Quote.C
+import qualified Language.C.Syntax as C
+
+import Language.C.Monad
+
+
+
+----------------------------------------------------------------------------------------------------
+-- * Interpretation of expressions
+----------------------------------------------------------------------------------------------------
+
+-- | General interface for evaluating expressions
+class EvalExp exp
+  where
+    -- | Predicate for literals
+    type LitPred exp :: * -> Constraint
+
+    -- | Literal expressions
+    litExp  :: LitPred exp a => a -> exp a
+
+    -- | Evaluation of (closed) expressions
+    evalExp :: exp a -> a
+
+-- | General interface for compiling expressions
+class CompExp exp
+  where
+    -- | Predicate for variables
+    type VarPred exp :: * -> Constraint
+
+    -- | Variable expressions
+    varExp  :: VarPred exp a => VarId -> exp a
+
+    -- | Compilation of expressions
+    compExp :: exp a -> CGen C.Exp
+
+type VarId = String
+
+
+
+----------------------------------------------------------------------------------------------------
+-- * Commands
+----------------------------------------------------------------------------------------------------
+
+data Ref a
+    = RefComp String
+    | RefEval (IORef a)
+
+-- | Commands for mutable references
+data RefCMD p exp a
+  where
+    NewRef          :: p a => RefCMD p exp (Ref a)
+    InitRef         :: p a => exp a -> RefCMD p exp (Ref a)
+    GetRef          :: p a => Ref a -> RefCMD p exp (exp a)
+    SetRef          :: p a => Ref a -> exp a -> RefCMD p exp ()
+    UnsafeFreezeRef :: p a => Ref a -> RefCMD p exp (exp a)
+
+
+
+----------------------------------------------------------------------------------------------------
+-- * Running commands
+----------------------------------------------------------------------------------------------------
+
+runRefCMD :: EvalExp exp => RefCMD (LitPred exp) exp a -> IO a
+runRefCMD (InitRef a)                   = fmap RefEval $ newIORef $ evalExp a
+runRefCMD NewRef                        = fmap RefEval $ newIORef (error "Reading uninitialized reference")
+runRefCMD (GetRef (RefEval r))          = fmap litExp  $ readIORef r
+runRefCMD (SetRef (RefEval r) a)        = writeIORef r $ evalExp a
+runRefCMD (UnsafeFreezeRef (RefEval r)) = fmap litExp  $ readIORef r
+
+
+
+----------------------------------------------------------------------------------------------------
+-- * Compiling commands
+----------------------------------------------------------------------------------------------------
+
+compTypeRep :: TypeRep -> C.Type
+compTypeRep trep = case show trep of
+    "Bool"  -> [cty| int   |]
+    "Int"   -> [cty| int   |]  -- todo: should only use fix-width Haskell ints
+    "Float" -> [cty| float |]
+
+typeOfP1 :: forall proxy a . Typeable a => proxy a -> TypeRep
+typeOfP1 _ = typeOf (undefined :: a)
+
+typeOfP2 :: forall proxy1 proxy2 a . Typeable a => proxy1 (proxy2 a) -> TypeRep
+typeOfP2 _ = typeOf (undefined :: a)
+
+class    (p1 a, p2 a) => (p1 :/\: p2) a
+instance (p1 a, p2 a) => (p1 :/\: p2) a
+
+compRefCMD :: CompExp exp => RefCMD (Typeable :/\: VarPred exp) exp a -> CGen a
+compRefCMD cmd@NewRef = do
+    let t = compTypeRep (typeOfP2 cmd)
+    sym <- gensym "r"
+    addLocal [cdecl| $ty:t $id:sym; |]
+    return $ RefComp sym
+compRefCMD cmd@(InitRef exp) = do
+    let t = compTypeRep (typeOfP2 cmd)
+    sym <- gensym "r"
+    v   <- compExp exp
+    addLocal [cdecl| $ty:t $id:sym; |]
+    addStm   [cstm| $id:sym = $v; |]
+    return $ RefComp sym
+compRefCMD cmd@(GetRef (RefComp ref)) = do
+    let t = compTypeRep (typeOfP2 cmd)
+    sym <- gensym "r"
+    addLocal [cdecl| $ty:t $id:sym; |]
+    addStm   [cstm| $id:sym = $id:ref; |]
+    return $ varExp sym
+compRefCMD (SetRef (RefComp ref) exp) = do
+    v <- compExp exp
+    addStm [cstm| $id:ref = $v; |]
+compRefCMD (UnsafeFreezeRef (RefComp ref)) = return $ varExp ref
+
+
+
+----------------------------------------------------------------------------------------------------
+-- * User interface
+----------------------------------------------------------------------------------------------------
+
+-- | Create an uninitialized reference
+newRef :: pred a => Program (RefCMD pred exp) (Ref a)
+newRef = singleton NewRef
+
+-- | Create an initialized reference
+initRef :: pred a => exp a -> Program (RefCMD pred exp) (Ref a)
+initRef = singleton . InitRef
+
+-- | Get the contents of reference
+getRef :: pred a => Ref a -> Program (RefCMD pred exp) (exp a)
+getRef r = singleton (GetRef r)
+
+-- | Set the contents of reference
+setRef :: pred a => Ref a -> exp a -> Program (RefCMD pred exp) ()
+setRef r = singleton . SetRef r
+
+-- | Modify the contents of reference
+modifyRef :: pred a => Ref a -> (exp a -> exp a) -> Program (RefCMD pred exp) ()
+modifyRef r f = getRef r >>= setRef r . f
+
+-- | Freeze the contents of reference (only safe if the reference is never accessed again)
+unsafeFreezeRef :: pred a => Ref a -> Program (RefCMD pred exp) (exp a)
+unsafeFreezeRef r = singleton (UnsafeFreezeRef r)
+
