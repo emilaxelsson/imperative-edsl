f62e44b9ef6bbd1cd76607d6eae9dc8ae86ea198
Emil Axelsson <emax@chalmers.se>
2015-03-10 13:20:18 +0100

    Make ALaCarte higher-order and poly-kinded

--- src/Data/ALaCarte.hs
+++ src/Data/ALaCarte.hs
@@ -1,19 +1,29 @@
 {-# LANGUAGE OverlappingInstances #-}
+{-# LANGUAGE PolyKinds #-}
+
+-- | Higher-order (and poly-kinded) implementation of Data Types à la Carte [1]
+--
+-- \[1\] W. Swierstra. Data Types à la Carte.
+--       /Journal of Functional Programming/, 18(4):423-436, 2008,
+--       <http://dx.doi.org/10.1017/S0956796808006758>.
 
 module Data.ALaCarte where
 
 
 
-data (f :+: g) a
-    = Inl (f a)
-    | Inr (g a)
+-- | Coproducts
+data (f :+: g) a b
+    = Inl (f a b)
+    | Inr (g a b)
   deriving (Functor)
 
 infixr :+:
 
+-- | A constraint @f `:<:` g@ expresses that the signature @f@ is subsumed by @g@, i.e. @f@ can be
+-- used to construct elements in @g@.
 class f :<: g
   where
-    inj :: f a -> g a
+    inj :: f a b -> g a b
 
 instance (f :<: f)
   where
