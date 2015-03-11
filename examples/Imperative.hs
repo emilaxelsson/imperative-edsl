c844d5672ca5fb204794a56f969968000f276366
Emil Axelsson <emax@chalmers.se>
2015-03-11 09:37:10 +0100

    More examles

--- examples/Imperative.hs
+++ examples/Imperative.hs
@@ -9,13 +9,23 @@ import Language.Embedded.Expr
 
 
 
-type Instr pred
+refProg :: pred Int => Program (Tag pred Expr (RefCMD pred Expr)) (Expr Int)
+refProg = do
+    r1 <- initRef 4
+    r2 <- initRef 5
+    a  <- unsafeFreezeRef r1
+    b  <- getRef r2
+    let c = Add a b
+    setRef r2 c
+    return c
+
+type CMD1 pred
     =   RefCMD pred Expr
     :+: ArrCMD pred Expr
     :+: ControlCMD Expr
 
-prog1 :: pred Int => Program (Tag pred Expr (Instr pred)) (Expr Int)
-prog1 = do
+arrProg :: pred Int => Program (Tag pred Expr (CMD1 pred)) (Expr Int)
+arrProg = do
     ref <- initRef 4
     arr <- newArr 10 6
     a   <- unsafeFreezeRef ref
@@ -26,8 +36,40 @@ prog1 = do
       (setRef ref b)
     return c
 
-eval1 :: IO Int
-eval1 = fmap evalExp $ interpret prog1
+evalRef :: IO Int
+evalRef = fmap evalExp $ interpret refProg
 
-comp1 = prettyCGen $ wrapMain $ interpret prog1
+compRef = prettyCGen $ wrapMain $ interpret refProg
+
+evalArr :: IO Int
+evalArr = fmap evalExp $ interpret arrProg
+
+compArr = prettyCGen $ wrapMain $ interpret arrProg
+
+
+
+type CMD2 pred
+    =   RefCMD pred Expr
+    :+: ControlCMD Expr
+    :+: FileCMD Expr
+    :+: ConsoleCMD Expr
+
+summer :: pred Float => Program (Tag pred Expr (CMD2 pred)) ()
+summer = do
+    inp <- open "input"
+    let cont = fmap Not $ feof inp
+    sum <- initRef 0
+    while cont $ do
+        f <- fget inp
+        s <- getRef sum
+        setRef sum (s+f)
+    s <- getRef sum
+    printf "The sum is: %f\n" s
+
+runSummer :: IO ()
+runSummer = do
+    writeFile "input" $ unwords $ map show ([-5..4] :: [Float])
+    interpret summer
+
+compSummer = prettyCGen $ wrapMain $ interpret summer
 
