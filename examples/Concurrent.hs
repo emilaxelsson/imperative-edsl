d23ac9c4bbb3f62888eb1e21f2987283972b0c7b
Anton Ekblad <anton@ekblad.cc>
2015-04-16 15:52:26 +0200

    Some concurrency examples.

---
+++ examples/Concurrent.hs
@@ -0,0 +1,51 @@
+module Main where
+import Language.Embedded.Imperative
+import Language.Embedded.Concurrent
+import Language.Embedded.Expr
+import Language.Embedded.Backend.C ()
+import Control.Applicative
+
+-- For compilation
+import Language.C.Monad
+
+type Pred = VarPred Expr
+
+type L =
+  ThreadCMD Expr :+:
+  ChanCMD Pred Expr :+:
+  ControlCMD Expr :+:
+  ConsoleCMD Expr :+:
+  FileCMD Expr
+
+-- | Deadlocks due to channel becoming full.
+deadlock :: Program L ()
+deadlock = do
+  c <- newChan 1
+  t <- fork $ readChan c >>= printf "%d\n"
+  writeChan c (1 :: Expr Int)
+  writeChan c 2
+  writeChan c 3
+  printf "This never happens: %d\n" (4 :: Expr Int)
+
+-- | Map a function over a file, then print the results. Mapping and printing
+--   happen in separate threads. Runs forever.
+mapFile :: (Expr Float -> Expr Float) -> FilePath -> Program L ()
+mapFile f i = do
+  c1 <- newChan 5
+  c2 <- newChan 5
+  t1 <- fork $ while (pure true) $ do
+    readChan c1 >>= writeChan c2 . f
+  t2 <- fork $ while (pure true) $ do
+    readChan c2 >>= printf "%f\n"
+  fi <- open i
+  while (Not <$> feof fi) $ do
+    fget fi >>= writeChan c1
+  close fi
+  waitThread t2
+
+-- | Waiting for thread completion.
+waiting :: Program L ()
+waiting = do
+  t <- fork $ printf "Forked thread printing %d\n" (0 :: Expr Int)
+  waitThread t
+  printf "Main thread printing %d\n" (1 :: Expr Int)
