51fd5ba91095d5a7d05a7f0113e353aa173ead81
Emil Axelsson <emax@chalmers.se>
2015-09-29 18:11:23 +0200

    Added a "semantics" test suite and a test for modifyRef

---
+++ tests/Semantics.hs
@@ -0,0 +1,22 @@
+import Language.Embedded.Expression (evalExp)
+import Language.Embedded.Imperative
+import Language.Embedded.Expr
+
+
+
+-- Test that `modifyRef` doesn't loop. It will loop if evaluation of `setRef` is
+-- too lazy so that the `unsafeFreezeRef` happens before `setRef` in
+-- `modifyRef`.
+modifyRefProg :: Program (RefCMD Expr) (Expr Int)
+modifyRefProg = do
+    r <- initRef 0
+    modifyRef r (+1)
+    getRef r
+
+testModifyRef = do
+    1 <- fmap evalExp $ interpret modifyRefProg
+    return ()
+
+main = do
+    testModifyRef
+
