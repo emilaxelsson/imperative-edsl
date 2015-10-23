3c98642bcd00239d2fb32ca1a5cefd5d9fb6d6b4
Josef Svenningsson <josef.svenningsson@gmail.com>
2015-10-23 11:51:20 +0200

    Add literal string arguments
    I've also added a test case which calls out to
    gcc to test that compilation works properly.

--- tests/Examples.hs
+++ tests/Examples.hs
@@ -1,5 +1,21 @@
 import Imperative ()
 import Concurrent ()
+import Language.Embedded.Expr
+import Language.Embedded.Imperative
+import System.IO
+import System.Process
+import System.Directory
+import System.Exit
 
-main = return ()
+main = do let c = compile $
+                 do addInclude "<stdio.h>" :: Program (CallCMD Expr) ()
+                    callProc "printf" [StrArg "Hello World!\n"]
+          (fp,h) <- openTempFile "" "temp.c"
+          hPutStrLn h c
+          hClose h
+          system $ "gcc " ++ fp
+          e <- system "./a.out"
+          removeFile fp
+          removeFile "a.out"
+          exitWith e
 
