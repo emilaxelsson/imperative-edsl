d67ecc0e51530ecd6e4233efd5bfa4c09d0acb1d
Emil Axelsson <emax@chalmers.se>
2015-12-18 22:19:35 +0100

    Use runCompiled in test

--- tests/Examples.hs
+++ tests/Examples.hs
@@ -3,20 +3,10 @@ import Concurrent ()
 import Language.Embedded.CExp
 import Language.Embedded.Imperative
 import Language.Embedded.Backend.C
-import System.IO
-import System.Process
-import System.Directory
-import System.Exit
 
-main = do let c = compile $
-                 do addInclude "<stdio.h>" :: Program (CallCMD CExp) ()
-                    callProc "printf" [strArg "Hello World!\n"]
-          (fp,h) <- openTempFile "" "temp.c"
-          hPutStrLn h c
-          hClose h
-          system $ "gcc " ++ fp
-          e <- system "./a.out"
-          removeFile fp
-          removeFile "a.out"
-          exitWith e
+test_strArg = flip (runCompiled []) [] $ do
+    addInclude "<stdio.h>" :: Program (CallCMD CExp) ()
+    callProc "printf" [strArg "Hello World!\n"]
+
+main = test_strArg
 
