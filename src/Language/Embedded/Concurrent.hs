a31843c3765eb8d058002bc2ec4b0a27cfb62837
Karácsony Máté <k_mate@inf.elte.hu>
2016-04-28 16:48:43 +0200

    Documentation + refactoring

--- src/Language/Embedded/Concurrent.hs
+++ src/Language/Embedded/Concurrent.hs
@@ -66,10 +66,20 @@ waitThread = singleton . inj . Wait
 
 class Transferable exp pred a
   where
+    -- | Size specification of a channel. In most of the cases, it is a natural
+    --   number representing how many elements could be stored at the same time
+    --   in the channel.
     type SizeSpec a :: *
 
+    -- | Maps a size specification to an internal channel size representation,
+    --   that is a map from primitive types to quantities. The byte size of the
+    --   channel will be calculated as the sum of multiplying the byte size of
+    --   each type with its quantity.
     calcChanSize :: pred a => proxy a -> SizeSpec a -> ChanSize exp pred
 
+    -- | Create a new channel. Writing a reference type to a channel will copy
+    --   contents into the channel, so modifying it post-write is completely
+    --   safe.
     newChan :: (Transferable exp pred a, pred a, ChanCMD :<: instr)
             => SizeSpec a
             -> ProgramT instr (Param2 exp pred) m (Chan Uncloseable a)
@@ -80,12 +90,21 @@ class Transferable exp pred a
                      -> ProgramT instr (Param2 exp pred) m (Chan Closeable a)
     newCloseableChan = singleInj . NewChan . calcChanSize (Proxy :: Proxy a)
 
+    -- | Read an element from a channel. If channel is empty, blocks until there
+    --   is an item available.
+    --   If 'closeChan' has been called on the channel *and* if the channel is
+    --   empty, @readChan@ returns an undefined value immediately.
     readChan :: ( pred a
                 , FreeExp exp, FreePred exp a
                 , ChanCMD :<: instr, Monad m )
              => Chan t a
              -> ProgramT instr (Param2 exp pred) m (exp a)
 
+    -- | Write a data element to a channel.
+    --   If 'closeChan' has been called on the channel, all calls to @writeChan@
+    --   become non-blocking no-ops and return @False@, otherwise returns @True@.
+    --   If the channel is full, this function blocks until there's space in the
+    --   queue.
     writeChan :: ( pred a
                  , FreeExp exp, FreePred exp Bool
                  , ChanCMD :<: instr, Monad m )
@@ -95,6 +114,10 @@ class Transferable exp pred a
 
 class Transferable exp pred a => BulkTransferable exp pred a
   where
+    -- | Read an arbitrary number of elements from a channel into an array.
+    --   The semantics are the same as for 'readChan', where "channel is empty"
+    --   is defined as "channel contains less data than requested".
+    --   Returns @False@ without reading any data if the channel is closed.
     readChanBuf :: ( pred a
                    , Ix i, Integral i
                    , FreeExp exp, FreePred exp Bool
@@ -105,6 +128,10 @@ class Transferable exp pred a => BulkTransferable exp pred a
                 -> Arr i a
                 -> ProgramT instr (Param2 exp pred) m (exp Bool)
 
+    -- | Write an arbitrary number of elements from an array into an channel.
+    --   The semantics are the same as for 'writeChan', where "channel is full"
+    --   is defined as "channel has insufficient free space to store all written
+    --   data".
     writeChanBuf :: ( Typeable a, pred a
                     , Ix i, Integral i
                     , FreeExp exp, FreePred exp Bool
@@ -114,6 +141,7 @@ class Transferable exp pred a => BulkTransferable exp pred a
                  -> exp i -- ^ Elements to write
                  -> Arr i a
                  -> ProgramT instr (Param2 exp pred) m (exp Bool)
+
 
 instance Transferable CExp CType a
   where
@@ -127,64 +155,6 @@ instance BulkTransferable CExp CType a
     readChanBuf  = readChanBuf'
     writeChanBuf = writeChanBuf'
 
-
---------------------------------------------------------------------------------
--- Channel primitives
---------------------------------------------------------------------------------
-
--- | Read an element from a channel. If channel is empty, blocks until there
---   is an item available.
---   If 'closeChan' has been called on the channel *and* if the channel is
---   empty, @readChan@ returns an undefined value immediately.
-readChan' :: ( Typeable a, pred a
-             , FreeExp exp, FreePred exp a
-             , ChanCMD :<: instr, Monad m )
-          => Chan t c
-          -> ProgramT instr (Param2 exp pred) m (exp a)
-readChan' = fmap valToExp . singleInj . ReadOne
-
--- | Read an arbitrary number of elements from a channel into an array.
---   The semantics are the same as for 'readChan', where "channel is empty"
---   is defined as "channel contains less data than requested".
---   Returns @False@ without reading any data if the channel is closed.
-readChanBuf' :: ( Typeable a, pred a
-                , Ix i, Integral i
-                , FreeExp exp, FreePred exp Bool
-                , ChanCMD :<: instr, Monad m )
-             => Chan t c
-             -> exp i -- ^ Offset in array to start writing
-             -> exp i -- ^ Elements to read
-             -> Arr i a
-             -> ProgramT instr (Param2 exp pred) m (exp Bool)
-readChanBuf' ch off sz arr = fmap valToExp . singleInj $ ReadChan ch off sz arr
-
--- | Write a data element to a channel.
---   If 'closeChan' has been called on the channel, all calls to @writeChan@
---   become non-blocking no-ops and return @False@, otherwise returns @True@.
---   If the channel is full, this function blocks until there's space in the
---   queue.
-writeChan' :: ( Typeable a, pred a
-              , FreeExp exp, FreePred exp Bool
-              , ChanCMD :<: instr, Monad m )
-           => Chan t c
-           -> exp a
-           -> ProgramT instr (Param2 exp pred) m (exp Bool)
-writeChan' c = fmap valToExp . singleInj . WriteOne c
-
--- | Write an arbitrary number of elements from an array into an channel.
---   The semantics are the same as for 'writeChan', where "channel is full"
---   is defined as "channel has insufficient free space to store all written
---   data".
-writeChanBuf' :: ( Typeable a, pred a
-                 , Ix i, Integral i
-                 , FreeExp exp, FreePred exp Bool
-                 , ChanCMD :<: instr, Monad m )
-              => Chan t c
-              -> exp i -- ^ Offset in array to start reading
-              -> exp i -- ^ Elements to write
-              -> Arr i a
-              -> ProgramT instr (Param2 exp pred) m (exp Bool)
-writeChanBuf' ch off sz arr = fmap valToExp . singleInj $ WriteChan ch off sz arr
 
 -- | When 'readChan' was last called on the given channel, did the read
 --   succeed?
@@ -202,3 +172,45 @@ closeChan :: (ChanCMD :<: instr)
           => Chan Closeable c
           -> ProgramT instr (Param2 exp pred) m ()
 closeChan = singleInj . CloseChan
+
+
+--------------------------------------------------------------------------------
+-- Channel primitives
+--------------------------------------------------------------------------------
+
+readChan' :: ( Typeable a, pred a
+             , FreeExp exp, FreePred exp a
+             , ChanCMD :<: instr, Monad m )
+          => Chan t c
+          -> ProgramT instr (Param2 exp pred) m (exp a)
+readChan' = fmap valToExp . singleInj . ReadOne
+
+readChanBuf' :: ( Typeable a, pred a
+                , Ix i, Integral i
+                , FreeExp exp, FreePred exp Bool
+                , ChanCMD :<: instr, Monad m )
+             => Chan t c
+             -> exp i -- ^ Offset in array to start writing
+             -> exp i -- ^ Elements to read
+             -> Arr i a
+             -> ProgramT instr (Param2 exp pred) m (exp Bool)
+readChanBuf' ch off sz arr = fmap valToExp . singleInj $ ReadChan ch off sz arr
+
+writeChan' :: ( Typeable a, pred a
+              , FreeExp exp, FreePred exp Bool
+              , ChanCMD :<: instr, Monad m )
+           => Chan t c
+           -> exp a
+           -> ProgramT instr (Param2 exp pred) m (exp Bool)
+writeChan' c = fmap valToExp . singleInj . WriteOne c
+
+writeChanBuf' :: ( Typeable a, pred a
+                 , Ix i, Integral i
+                 , FreeExp exp, FreePred exp Bool
+                 , ChanCMD :<: instr, Monad m )
+              => Chan t c
+              -> exp i -- ^ Offset in array to start reading
+              -> exp i -- ^ Elements to write
+              -> Arr i a
+              -> ProgramT instr (Param2 exp pred) m (exp Bool)
+writeChanBuf' ch off sz arr = fmap valToExp . singleInj $ WriteChan ch off sz arr
